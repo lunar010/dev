c955914dc2d6a2be5c44963a993e7b52650121b7
EclipseBETA <aEclipse@outlook.kr>
2022-05-01 11:30:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,23 +4,6 @@ web = Flask(__name__)
 
 server_domain = 'cloudlayer.kro.kr'
 
-@web.route('/', defaults={'path': ''})
-@web.route('/<path:path>')
-def main(path):
-    url_splited = request.url.split("/")[2]
-    if url_splited == server_domain:
-        subdomains = ''
-    else:
-        subdomains = request.url.split("/")[2].replace(server_domain, '')
-        resp = requests.request(
-            method=request.method,
-            url='https://' + subdomains + 'xates.dev/'+path,
-            headers={key: value for (key, value) in request.headers if key != 'Host'},
-            data=request.get_data(),
-            cookies=request.cookies,
-            allow_redirects=False)
-        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-        headers = [(name, value) for (name, value) in resp.raw.headers.items()
-                 if name.lower() not in excluded_headers]
-        response = Response(resp.content, resp.status_code, headers)
-        return response
+@web.route('/')
+def home():
+    return 'hi'
